46ef390be2e3ae59bbefec2f47e4a301048553f2
Manthan Ank <manthan.ank46@gmail.com>
2023-02-11 15:13:33 +0530

    changed name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-firestore'`, () => {
+  it(`should have as title 'eztaskio-assignment'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-firestore');
+    expect(app.title).toEqual('eztaskio-assignment');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-firestore app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('eztaskio-assignment app is running!');
   });
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'angular-firestore-3ed06',
+    projectId: 'eztaskio-assignment-3ed06',
     appId: '1:691602437416:web:23350b0c3bee10a2108c1d',
-    databaseURL: 'https://angular-firestore-3ed06-default-rtdb.firebaseio.com',
-    storageBucket: 'angular-firestore-3ed06.appspot.com',
+    databaseURL: 'https://eztaskio-assignment-3ed06-default-rtdb.firebaseio.com',
+    storageBucket: 'eztaskio-assignment-3ed06.appspot.com',
     locationId: 'us-central',
     apiKey: 'AIzaSyD9V88CLLkz67IB_mRlaTyFNuMe85MuQ14',
-    authDomain: 'angular-firestore-3ed06.firebaseapp.com',
+    authDomain: 'eztaskio-assignment-3ed06.firebaseapp.com',
     messagingSenderId: '691602437416',
     measurementId: 'G-3909ZYMLMC',
   },
